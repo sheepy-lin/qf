6b99a929b3abe9ebd0cb0eff3eaa10deb1f48349
sheepy-lin <sheepyoffice@gmail.com>
2022-04-27 00:28:07 +0800

    0427初次完稿

--- js/custom.js
+++ js/custom.js
@@ -104,10 +104,10 @@ $(document).ready(function () {
   //選單
 
   var menuCon = $(".menuCon")
-  var subButton=$(".button")
-  var classesListWrap=$(".classesListWrap")
-  var searButtonShow=$(".searButtonShow")
-  var searchBar=$(".searchBarWrap")
+  var subButton = $(".button")
+  var classesListWrap = $(".classesListWrap")
+  var searchButtonShow = $(".searchButtonShow")
+  var searchBar = $(".searchBarWrap")
 
   // menuCon.hide();
   classesListWrap.hide();
@@ -115,7 +115,7 @@ $(document).ready(function () {
 
 
   function global() {
-    
+
     var windowWidth = screen.width
 
     if (windowWidth < 1024) {
@@ -125,6 +125,13 @@ $(document).ready(function () {
       var menuState = false;
 
       menuBtnDefault.click(openMenuForM)
+      
+      //案例作品次選單
+
+      subButton.click(function () {
+        classesListWrap.stop().slideToggle("slow");
+        console.log($(window).width())
+      })
 
       function openMenuForM() {
 
@@ -132,7 +139,7 @@ $(document).ready(function () {
 
           console.log("開")
 
-          $(".menuCon").fadeIn(800).css("display" , "flex")
+          menuCon.fadeIn(800).css("display", "flex")
           menuBtnDefault.css("z-index", "1000").removeClass("Default").addClass("Close")
           menuWord.addClass("XX")
 
@@ -140,7 +147,7 @@ $(document).ready(function () {
           $(".close_1").addClass("XX_line_1 XX_postion")
           $(".close_2").addClass("XX_line_2 XX_postion").show();
 
-          
+
           menuState = true;
           $("body").addClass("scrollbarNone")
           console.log("確認開啟")
@@ -150,8 +157,8 @@ $(document).ready(function () {
 
           console.log("關")
 
-          $(".menuCon").fadeOut(800).css("display", "none")
-          menuBtnDefault.css("z-index", "0").addClass("Default").removeClass("Close")
+          menuCon.css("display","flex").fadeOut(800)
+          menuBtnDefault.css("z-index", "0").addClass("Default").addClass("Close")
           menuWord.removeClass("XX")
 
           $(".line:nth-child(1),.line:nth-child(4)").show();
@@ -159,24 +166,22 @@ $(document).ready(function () {
           $(".close_2").removeClass("XX_line_2 XX_postion").hide();
 
           menuState = false;
+
           console.log("確認關閉")
           $("body").removeClass("scrollbarNone")
+
 
         }
       }
 
-      //案例作品次選單
-
-      subButton.click(function () {
-        classesListWrap.stop().slideToggle("slow");
-       
-      });
+      ;
 
       //搜尋bar點選
 
-      searButtonShow.click(function () {
+      searchButtonShow.click(function () {
         searchBar.stop().slideToggle("800");
-        
+
+
       });
 
     } else {
@@ -185,45 +190,48 @@ $(document).ready(function () {
       var menuWord = $(".menu")
       var header = $("header")
       var menuState = false;
-      
-      
+
+
 
       $(".line:nth-child(1),.line:nth-child(4)").show();
       $(".close_1").removeClass("XX_line_1 XX_postion")
-      $(".close_2").removeClass("XX_line_2 XX_postion").hide();
+      // $(".close_2").removeClass("XX_line_2 XX_postion").hide();
 
-      header.addClass("lineMoveToClose")
+      // header.addClass("lineMoveToClose")
 
 
       function openMenuForD() {
         if (menuState == false) {
-          
+
           console.log("開")
 
-          $(".menuCon").removeClass("close").addClass("open").css("display", "flex")
+          // menuCon.removeClass("close").addClass("open").css("display", "flex")
+          menuCon.css("display", "flex")
           menuBtnDefault.css("z-index", "1000").removeClass("Default").addClass("Close")
           menuWord.addClass("XX")
 
-          $(".line:nth-child(1),.line:nth-child(4)").hide();
-          $(".close_1").addClass("XX_line_1 XX_postion")
-          $(".close_2").addClass("XX_line_2 XX_postion").show();
+          $(".line:nth-child(1),.line:nth-child(4)").fadeOut(800);
+          $(".close_1").addClass("XX_line_1 XX_postion").removeClass("XX_line_1_reverse");
+          $(".close_2").fadeIn().addClass("XX_line_2 XX_postion").removeClass("XX_line_2_reverse");;
 
           header.removeClass("lineMoveToClose").addClass("lineMoveToOpen")
 
           menuState = true;
           console.log("確定開啟")
+          // classesListWrap.hide();
 
         } else {
 
           console.log("關")
 
-          $(".menuCon").removeClass("open").addClass("close").css("display", "none")
-          menuBtnDefault.css("z-index", "0").addClass("Default").removeClass("Close")
+          // menuCon.removeClass("open").addClass("close")
+          menuCon.css("display", "flex")
+          menuBtnDefault.css("z-index", "0").addClass("Default")
           menuWord.removeClass("XX")
 
-          $(".line:nth-child(1),.line:nth-child(4)").show();
-          $(".close_1").removeClass("XX_line_1 XX_postion")
-          $(".close_2").removeClass("XX_line_2 XX_postion").hide();
+          $(".line:nth-child(1),.line:nth-child(4)").fadeIn(800);
+          $(".close_1").addClass("XX_line_1_reverse").removeClass("XX_line_1 XX_postion");
+          $(".close_2").addClass("XX_line_2_reverse").removeClass("XX_line_2").fadeOut(800);
 
           header.removeClass("lineMoveToOpen").addClass("lineMoveToClose")
 
@@ -235,12 +243,15 @@ $(document).ready(function () {
 
       menuBtnDefault.click(openMenuForD)
       classesListWrap.show();
+
     }
+
   }
 
 
   global()
   $(window).resize(global)
+
 
   //呼叫地圖
 
@@ -264,4 +275,28 @@ $(document).ready(function () {
     $("body").addClass("scrollbarNone")
   }
 
+  
+
+  function serviceBtn(){
+    var btn =$(this)
+    var nowCllickSort = btn.index()
+    var list = $(".sideServiceList ul li")
+    var thum = $(".thum")
+    var img = $(".serviceImg img")
+    var conList = $(".sideServiceConList")
+    
+    list.removeClass("drColor").eq(nowCllickSort).addClass("drColor")
+
+    thum.removeClass("drBg").eq(nowCllickSort).addClass("drBg")
+
+    img.removeClass("drOpacity").eq(nowCllickSort).addClass("drOpacity")
+
+    conList.removeClass("conOpacity").css("display","none").eq(nowCllickSort).addClass("conOpacity").css("display","block")
+
+    console.log(nowCllickSort)
+  }
+
+  $(".sideServiceList ul li").click(serviceBtn)
+  $(".thum").click(serviceBtn)
+  $(".serviceItem").click(serviceBtn)
 })
